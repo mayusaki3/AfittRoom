6c3c52c43ed48d2349a9d45aa090a5d7b24d0bb6
mayusaki3 <masayuki@fa2.so-net.ne.jp>
2021-04-24 01:48:04 +0900

    glTFスキーマに合わせて修正。

---
+++ src/Sansa.Model/Format/glTF/glTF.Camera.OrthoGraphic.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+
+namespace Sansa.Model.Format
+{
+    public partial class GLTF
+    {
+        public partial class Camera
+        {
+            /// <summary>
+            /// カメラの正射投影
+            /// <br/>正射投影マトリクスを作成するためのプロパティを持つ正射投影カメラです。
+            /// </summary>
+            /// <remarks>
+            /// https://github.com/KhronosGroup/glTF/blob/master/specification/2.0/schema/camera.orthographic.schema.json
+            /// </remarks>
+            public partial class OrthoGraphic
+            {
+#pragma warning disable IDE1006 // 命名スタイル
+
+                /// <summary>
+                /// ビューの水平方向の倍率
+                /// <br/>浮動小数点で指定します。
+                /// <br/>ゼロであってはなりません。
+                /// </summary>
+                /// <remarks>
+                /// 必須項目
+                /// </remarks>
+                public double? xmag { get; set; } = null;
+
+                /// <summary>
+                /// ビューの垂直方向の倍率
+                /// <br/>浮動小数点で指定します。
+                /// <br/>ゼロであってはなりません。
+                /// </summary>
+                /// <remarks>
+                /// 必須項目
+                /// </remarks>
+                public double? ymag { get; set; } = null;
+
+                /// <summary>
+                /// 遠くのクリッピング・プレーンまでの距離
+                /// <br/>浮動小数点で指定します。
+                /// <br/>zfar は znear よりも大きくなければなりません。
+                /// </summary>
+                /// <remarks>
+                /// 必須項目
+                /// <br/>最小 = 0.0
+                /// <br/>最小値を含まない
+                /// </remarks>
+                public double? zfar { get; set; } = null;
+
+                /// <summary>
+                /// 近接クリッピング・プレーンまでの距離
+                /// <br/>浮動小数点で指定します。
+                /// </summary>
+                /// <remarks>
+                /// 必須項目
+                /// <br/>最小 = 0.0
+                /// </remarks>
+                public double? znear { get; set; } = null;
+
+                /// <summary>
+                /// エクステンション
+                /// </summary>
+                public List<Extension> extensions { get; set; } = null;
+
+                /// <summary>
+                /// アプリケーション固有のデータ
+                /// </summary>
+                public Extras extras { get; set; } = null;
+
+#pragma warning restore IDE1006 // 命名スタイル
+            }
+        }
+    }
+}
---
+++ src/Sansa.Model/Format/glTF/glTF.Camera.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+
+namespace Sansa.Model.Format
+{
+    public partial class GLTF
+    {
+        /// <summary>
+        /// カメラ
+        /// <br/>カメラの投影。
+        /// <br/>ノードはカメラを参照して、シーン内にカメラを配置するためのトランスフォームを適用することができます。
+        /// </summary>
+        /// <remarks>
+        /// https://github.com/KhronosGroup/glTF/blob/master/specification/2.0/schema/camera.schema.json
+        /// </remarks>
+        public partial class Camera
+        {
+#pragma warning disable IDE1006 // 命名スタイル
+
+            /// <summary>
+            /// 正射投影マトリクスを作成するためのプロパティを持つ正射投影カメラ
+            /// </summary>
+            /// <remarks>
+            /// 必須項目ではない
+            /// </remarks>
+            public OrthoGraphic orthographic { get; set; } = null;
+
+            /// <summary>
+            /// 透視投影マトリクスを作成するためのプロパティを持つ透視投影カメラ
+            /// </summary>
+            /// <remarks>
+            /// 必須項目ではない
+            /// </remarks>
+            public Perspective perspective { get; set; } = null;
+
+            /// <summary>
+            /// カメラが透視投影か正射投影かを指定
+            /// </summary>
+            public enum @Type
+            {
+                perspective,
+
+                orthographic
+            }
+
+            /// <summary>
+            /// カメラが透視投影か正射投影かを指定
+            /// これに基づいて、カメラの perspective または orthographic プロパティが定義されます。
+            /// </summary>
+            /// <remarks>
+            /// 必須項目
+            /// </remarks>
+            public Type? type { get; set; } = null;
+
+            /// <summary>
+            /// 名前
+            /// <br/>このカメラの名前です。
+            /// </summary>
+            public string name { get; set; } = null;
+
+            /// <summary>
+            /// エクステンション
+            /// </summary>
+            public List<Extension> extensions { get; set; } = null;
+
+            /// <summary>
+            /// アプリケーション固有のデータ
+            /// </summary>
+            public Extras extras { get; set; } = null;
+
+#pragma warning restore IDE1006 // 命名スタイル
+        }
+    }
+}
