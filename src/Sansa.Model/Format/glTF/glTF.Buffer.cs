dfdae2592e35fc761e7c2b07dce39ee261f05874
mayusaki3 <masayuki@fa2.so-net.ne.jp>
2021-04-24 00:39:48 +0900

    glTFスキーマに合わせて修正。

---
+++ src/Sansa.Model/Format/glTF/glTF.Buffer.cs
@@ -0,0 +1,36 @@
+﻿namespace Sansa.Model.Format
+{
+    public partial class GLTF
+    {
+        /// <summary>
+        /// バッファ
+        /// <br/>バイナリのジオメトリ、アニメーション、スキンを指します。
+        /// </summary>
+        /// <remarks>
+        /// https://github.com/KhronosGroup/glTF/blob/master/specification/2.0/schema/buffer.schema.json
+        /// </remarks>
+        public class Buffer
+        {
+#pragma warning disable IDE1006 // 命名スタイル
+
+            /// <summary>
+            /// バッファのURI
+            /// <br/>相対パスは、.gltfファイルからの相対パスです。
+            /// <br/>外部ファイルを参照する代わりに、uriはdata-uriにすることもできます。
+            /// </summary>
+            public string uri { get; set; } = null;
+
+            /// <summary>
+            /// バッファの長さ
+            /// <br/>バイト単位で指定します。
+            /// </summary>
+            /// <remarks>
+            /// 必須項目
+            /// <br/>最小 = 1
+            /// </remarks>
+            public int? byteLength { get; set; } = null;
+
+#pragma warning restore IDE1006 // 命名スタイル
+        }
+    }
+}
