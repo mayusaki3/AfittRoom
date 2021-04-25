cffce34aa2699b439a7e0f33d9d556687e934161
mayusaki3 <masayuki@fa2.so-net.ne.jp>
2021-04-25 16:34:44 +0900

    階層変更に合わせて変更。

--- src/Sansa.Model/Avatar.cs
+++ src/Sansa.Model/Avatar.cs
@@ -22,36 +22,8 @@ namespace Sansa.Model
         public void Load(List<GLB_Chunk> ChunkList)
         {
             // チャンク0を処理
-            using var ms = new MemoryStream(ChunkList[0].ChunkData, 0, (int)ChunkList[0].ChunkLength);
-            using var rd = new StreamReader(ms, Encoding.UTF8);
-            string js = rd.ReadToEnd();
+            avatarTF.Parse(ChunkList[0]);
 
-            string fnam = @"C:\WORKPLACE\VRoom\LOAD.txt";
-            using (StreamWriter sw = new(fnam))
-            {
-                sw.WriteLine(js.Replace(",", ",\r\n"));
-                Logging.Write("入力VRMのチャンク0(JSON)を " + fnam + " に出力しました。");
-            }
-
-            SetJsonSerializerOptions(out JsonSerializerOptions opt);
-
-            avatarTF.schema = JsonSerializer.Deserialize<AvatarTF.Schema>(js, opt);
-
-            Logging.Write("***VRM情報***");
-            Logging.Write("[title]: " + avatarTF.schema.extensions.VRM.meta.title);
-            Logging.Write("[author]: " + avatarTF.schema.extensions.VRM.meta.author);
-
-            AvatarTF.EnumAttribute ea = avatarTF.GetEnumAttr<AvatarTF.Meta.AllowedUserName>(avatarTF.schema.extensions.VRM.meta.allowedUserName);
-            Logging.Write("[allowedUserName]: " + ea.display + "  // " + ea.description);
-
-            ea = avatarTF.GetEnumAttr<AvatarTF.Meta.AllowOrDisallow>(avatarTF.schema.extensions.VRM.meta.violentUssageName);
-            Logging.Write("[violentUssageName]: " + ea.display);
-
-            ea = avatarTF.GetEnumAttr<AvatarTF.Meta.AllowOrDisallow>(avatarTF.schema.extensions.VRM.meta.sexualUssageName);
-            Logging.Write("[sexualUssageName]: " + ea.display);
-
-            ea = avatarTF.GetEnumAttr<AvatarTF.Meta.LicenseName>(avatarTF.schema.extensions.VRM.meta.licenseName);
-            Logging.Write("[licenseName]: " + ea.display + "  // " + ea.description);
 
         }
 
@@ -68,51 +40,13 @@ namespace Sansa.Model
             ChunkList = new();
 
             // GLBチャンク0作成
-            GLB_Chunk chunk0 = new();
-            chunk0.ChunkType = GLB_Chunk.ChankType.JSON;
+            avatarTF.ToChunk(out GLB_Chunk chunk0);
 
-            SetJsonSerializerOptions(out JsonSerializerOptions opt);
-
-            string js = JsonSerializer.Serialize(avatarTF.schema, opt);
-            byte[] jsbytes = System.Text.Encoding.UTF8.GetBytes(js);
-            chunk0.ChunkLength = (uint)(jsbytes.Length + jsbytes.Length % 4);
-            chunk0.ChunkData = new byte[chunk0.ChunkLength];
-            jsbytes.CopyTo(chunk0.ChunkData, 0);
-            for (int i = jsbytes.Length; i < chunk0.ChunkLength; i++) chunk0.ChunkData[i] = 0x20;
             ChunkList.Add(chunk0);
-
-
-            string fnam = @"C:\WORKPLACE\VRoom\SAVE.txt";
-            using StreamWriter sw = new(fnam);
-            sw.WriteLine(js.Replace(",", ",\r\n"));
-            Logging.Write("出力VRMのチャンク0(JSON)を " + fnam + " に出力しました。");
-
         }
 
         #endregion
-
-        #region アバターセーブ (Save)
-
-        /// <summary>
-        /// GLBチャンクリストにアバターをセーブする。
-        /// </summary>
-        /// <param name="ChunkList"></param>
-        private void SetJsonSerializerOptions(out JsonSerializerOptions option)
-        {
-            option = new()
-            {
-                DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
-            };
-            option.Converters.Add(new AvatarTF.JsonConverterForNullableInt());
-            option.Converters.Add(new AvatarTF.JsonConverterForNullableDouble());
-            option.Converters.Add(new AvatarTF.JsonConverterForNullableEnum<AvatarTF.Meta.AllowedUserName>());
-            option.Converters.Add(new AvatarTF.JsonConverterForNullableEnum<AvatarTF.Meta.AllowOrDisallow>());
-            option.Converters.Add(new AvatarTF.JsonConverterForNullableEnum<AvatarTF.Meta.LicenseName>());
-            option.Converters.Add(new AvatarTF.JsonConverterForNullableEnumValue<AvatarTF.Accessor.ComponentType>());
-            option.Converters.Add(new AvatarTF.JsonConverterForNullableEnum<AvatarTF.Accessor.AttributeType>());
-        }
-
-        #endregion
+ 
 
         #endregion
 
--- src/Sansa.Model/AvatarTF.cs
+++ src/Sansa.Model/AvatarTF.cs
@@ -1,14 +1,135 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Reflection;
+﻿using Jaffa.Diagnostics;
+using Sansa.Model.Format;
+using Sansa.Model.FormatHelper;
+using System.IO;
+using System.Text;
+using System.Text.Encodings.Web;
 using System.Text.Json;
+using static Sansa.Model.Format.glTF2.Extention;
 
 namespace Sansa.Model
 {
     public partial class AvatarTF
     {
-        #region
+        #region メソッド
+
+        #region GLBチャンク0(JSON)のパース (Parse)
+
+
+        /// <summary>
+        /// GLBチャンク0(JSON)のパース
+        /// </summary>
+        /// <param name="chunk0">GLBチャンク0</param>
+        public void Parse(GLB_Chunk chunk0)
+        {
+            using var ms = new MemoryStream(chunk0.ChunkData, 0, (int)chunk0.ChunkLength);
+            using var rd = new StreamReader(ms, Encoding.UTF8);
+            string js = rd.ReadToEnd();
+
+
+            string fnam = @"C:\WORKPLACE\VRoom\LOAD.txt";
+            using (StreamWriter sw = new(fnam))
+            {
+                sw.WriteLine(js.Replace(",", ",\r\n"));
+                Logging.Write("入力VRMのチャンク0(JSON)を " + fnam + " に出力しました。");
+            }
+
+            SetJsonSerializerOptions(out JsonSerializerOptions opt);
+
+            //var jsonElems = JsonDocument.Parse(text);
+            //var elmLength = jsonElems.RootElement.GetProperty(TotalItems).GetInt32();
+
+
+
+            //JsonDocument jsdoc = JsonDocument.Parse(js);
+
+
+            //foreach (JsonElement je in jsdoc.RootElement.EnumerateArray())
+            //{
+            //    System.Diagnostics.Debug.Print(je.GetString());
+
+
+            //}
+
+
+
+            //object obj = JsonSerializer.Deserialize(js, opt);
+
+            schema = JsonSerializer.Deserialize<glTF2>(js, opt);
+
+            //Logging.Write("***VRM情報***");
+            //Logging.Write("[title]: " + avatarTF.schema.extensions.VRM.meta.title);
+            //Logging.Write("[author]: " + avatarTF.schema.extensions.VRM.meta.author);
+
+            //AvatarTF.EnumAttribute ea = avatarTF.GetEnumAttr<AvatarTF.Meta.AllowedUserName>(avatarTF.schema.extensions.VRM.meta.allowedUserName);
+            //Logging.Write("[allowedUserName]: " + ea.display + "  // " + ea.description);
+
+            //ea = avatarTF.GetEnumAttr<AvatarTF.Meta.AllowOrDisallow>(avatarTF.schema.extensions.VRM.meta.violentUssageName);
+            //Logging.Write("[violentUssageName]: " + ea.display);
+
+            //ea = avatarTF.GetEnumAttr<AvatarTF.Meta.AllowOrDisallow>(avatarTF.schema.extensions.VRM.meta.sexualUssageName);
+            //Logging.Write("[sexualUssageName]: " + ea.display);
+
+            //ea = avatarTF.GetEnumAttr<AvatarTF.Meta.LicenseName>(avatarTF.schema.extensions.VRM.meta.licenseName);
+            //Logging.Write("[licenseName]: " + ea.display + "  // " + ea.description);
+        }
+
+        #endregion
+
+        #region GLBチャンク0(JSON)に変換 (ToChunk)
+
+        /// <summary>
+        /// GLBチャンク0(JSON)に変換
+        /// </summary>
+        /// <param name="chunk0">GLBチャンク0</param>
+        public void ToChunk(out GLB_Chunk chunk0)
+        {
+            chunk0 = new();
+            chunk0.ChunkType = GLB_Chunk.ChankType.JSON;
+
+            SetJsonSerializerOptions(out JsonSerializerOptions opt);
+
+            string js = JsonSerializer.Serialize(schema, opt);
+            byte[] jsbytes = Encoding.UTF8.GetBytes(js);
+            chunk0.ChunkLength = (uint)(jsbytes.Length + jsbytes.Length % 4);
+            chunk0.ChunkData = new byte[chunk0.ChunkLength];
+            jsbytes.CopyTo(chunk0.ChunkData, 0);
+            for (int i = jsbytes.Length; i < chunk0.ChunkLength; i++) chunk0.ChunkData[i] = 0x20;
+
+
+            string fnam = @"C:\WORKPLACE\VRoom\SAVE.txt";
+            using StreamWriter sw = new(fnam);
+            sw.WriteLine(js.Replace(",", ",\r\n"));
+            Logging.Write("出力VRMのチャンク0(JSON)を " + fnam + " に出力しました。");
+
+        }
+
+        #endregion
+
+        #region JSONシリアル化のためのオプションを設定 (SetJsonSerializerOptions) [private]
+
+        /// <summary>
+        /// JSONシリアル化のためのオプションを設定
+        /// </summary>
+        /// <param name="ChunkList"></param>
+        private void SetJsonSerializerOptions(out JsonSerializerOptions option)
+        {
+            option = new()
+            {
+                DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+            };
+            option.Converters.Add(new JsonConverterForNullableFloat());
+            option.Converters.Add(new JsonConverterForNullableEnum<VRM0.Meta.AllowedUserName>());
+            option.Converters.Add(new JsonConverterForNullableEnum<VRM0.Meta.AllowOrDisallow>());
+            option.Converters.Add(new JsonConverterForNullableEnum<VRM0.Meta.LicenseName>());
+            option.Converters.Add(new JsonConverterForNullableEnumValue<glTF2.Accessor.ComponentType>());
+            option.Converters.Add(new JsonConverterForNullableEnum<glTF2.Accessor.AttributeType>());
+            option.Converters.Add(new JsonConverterForNullableEnumValueString<glTF2.Image.MimeType>());
+            option.Converters.Add(new JsonConverterForNullableEnum<glTF2.Material.AlphaMode>());
+        }
+
+        #endregion
 
         #region
 
@@ -25,23 +146,7 @@ namespace Sansa.Model
 
         #endregion
 
-        #region
 
-        /// <summary>
-        /// TODO: チェッカーどうする？
-        /// 必須項目チェック
-        /// </summary>
-        public EnumAttribute GetEnumAttr<T>(T? value) where T : struct, Enum
-        {
-            EnumAttribute rt = new();
-            FieldInfo fld = typeof(T).GetField(Enum.GetName(typeof(T), value));
-            EnumAttribute ea = (EnumAttribute)fld.GetCustomAttribute(typeof(EnumAttribute));
-            rt.display = Core.MakeMessage(ea.display);
-            rt.description = Core.MakeMessage(ea.description);
-            return rt;
-        }
-
-        #endregion
 
 
         #endregion
@@ -50,13 +155,11 @@ namespace Sansa.Model
 
         #region
 
-#pragma warning disable IDE1006 // 命名スタイル
         /// <summary>
         /// TODO: 説明
         /// </summary>
-        public Schema schema { get; set; }
+        public glTF2 schema { get; set; }
 
-#pragma warning restore IDE1006 // 命名スタイル
 
         #endregion
 
